552dd69db838889578cc5c88c538f261472c7b1b
vandat3012 <luongvandat301220@gmail.com>
2023-11-22 08:35:18 +0700

    update bai tap

--- feet-meter.js
+++ feet-meter.js
@@ -6,7 +6,6 @@ function feetToMeter(feet) {
     return 0.305 * feet;
 }
 let feet =+prompt("enter foot:");
-document.write(`${feet} foot = ${feetToMeter(feet)} meter`);
-document.write ("<br>")
+document.write(`${feet} foot = ${feetToMeter(feet)} meter <br>`);
 let meter = +prompt("enter meter:");
 document.write(`${meter} meter = ${meterToFeet(meter)} foot`);
